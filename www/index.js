671f99059d43f12ce90db495b14dcd7c5510f2a2
Vijay <vijay@iavian.com>
2023-08-07 12:41:36 +0530

    Init

--- www/index.js
+++ www/index.js
@@ -1,12 +1,56 @@
-async function run_wasm() {
+window.onload = (event) => {
     let myWorker = new Worker('./worker.js')
-    document.getElementById("file_picker").addEventListener("change", function () {
-        let file = this.files[0];
+    let dropArea = document.getElementById("drop-area")
+
+
+        ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+            dropArea.addEventListener(eventName, preventDefaults, false)
+            document.body.addEventListener(eventName, preventDefaults, false)
+        })
+
+        ;['dragenter', 'dragover'].forEach(eventName => {
+            dropArea.addEventListener(eventName, highlight, false)
+        })
+
+        ;['dragleave', 'drop'].forEach(eventName => {
+            dropArea.addEventListener(eventName, unhighlight, false)
+        })
+
+    dropArea.addEventListener('drop', handleDrop, false)
+
+    function handleDrop(e) {
+        let dt = e.dataTransfer
+        let files = dt.files
+        let file = files[0];
+        handleFile(file)
+    }
+
+    function handleFile(file) {
         myWorker.postMessage({ file: file });
         myWorker.onmessage = function (e) {
             console.dir(e.data)
             alert(JSON.stringify(e.data))
         }
+    }
+    
+    document.getElementById("file_picker").addEventListener("change", function () {
+        let file = this.files[0];
+        handleFile(file)
     })
+
 }
-run_wasm();
+
+function preventDefaults(e) {
+    e.preventDefault()
+    e.stopPropagation()
+}
+
+function highlight(e) {
+    let dropArea = document.getElementById("drop-area")
+    dropArea.classList.add('highlight')
+}
+
+function unhighlight(e) {
+    let dropArea = document.getElementById("drop-area")
+    dropArea.classList.remove('highlight')
+}
